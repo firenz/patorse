305ec95ac840c3c24e9e20fe68dd17c750799f70
Alicia G <alicia.guardenoalbertos@gmail.com>
2020-05-26 20:50:01 +0200

    add morse audio code

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,18 +1,20 @@
 import convertMessage from "./morse-conversion";
+import { playShortAudio } from "./morse-audio";
 
 const startPatorse = (): void => {
-  let btn = (<HTMLInputElement>document.getElementById("cuackbutton"));
+  let btn = (<HTMLInputElement>document.getElementById("cuack-btn"));
   btn.addEventListener("click", (e: Event) => {
     handleInput();
-    btn.disabled = true;
+    // btn.disabled = true;
   });
 };
 
 const handleInput = (): void => {
-  const message = (<HTMLInputElement>document.getElementById("stringToMorse")).value;
+  const message = (<HTMLInputElement>document.getElementById("message")).value;
   const convertedMessage = convertMessage(message);
   console.log(message);
   console.log(convertedMessage);
+  playShortAudio();
 };
 
 startPatorse();
---
+++ src/ts/morse-audio.ts
@@ -0,0 +1,77 @@
+const shortAudio1 = require("../media/audio/short/C1.wav");
+const shortAudio2 = require("../media/audio/short/C2.wav");
+const shortAudio3 = require("../media/audio/short/C3.wav");
+const shortAudio4 = require("../media/audio/short/C4.wav");
+
+const longAudio1 = require("../media/audio/long/L1.wav");
+const longAudio2 = require("../media/audio/long/L2.wav");
+const longAudio3 = require("../media/audio/long/L3.wav");
+const longAudio4 = require("../media/audio/long/L4.wav");
+
+interface SoundsCollection {
+  [key: number]: any;
+}
+
+const shortSounds: SoundsCollection = {
+  0: shortAudio1,
+  1: shortAudio2,
+  2: shortAudio3,
+  3: shortAudio4
+}
+
+const longSounds: SoundsCollection = {
+  0: longAudio1,
+  1: longAudio2,
+  2: longAudio3,
+  3: longAudio4
+}
+
+// for legacy browsers
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const audioContext = new AudioContext();
+
+// get the audio element
+const audioElement = <HTMLAudioElement>document.getElementById("cuack-audio");
+
+// pass it into the audio context
+const track = audioContext.createMediaElementSource(audioElement);
+
+// volume
+const gainNode = audioContext.createGain();
+
+const volumeControl = <HTMLInputElement>document.getElementById("volume");
+volumeControl.addEventListener(
+  "input",
+  function () {
+    gainNode.gain.value = this.value;
+  },
+  false
+);
+
+track.connect(gainNode).connect(audioContext.destination);
+
+const playAudio = (sounds: SoundsCollection): void => {
+  const randomNumber = getRandomInt(0, Object.keys(sounds).length);
+  console.log(sounds[randomNumber]);
+  audioElement.src = sounds[randomNumber];
+
+  // check if context is in suspended state (autoplay policy)
+  if (audioContext.state === "suspended") {
+    audioContext.resume();
+  }
+
+  audioElement.play();
+};
+
+const getRandomInt = (min: number, max: number): number => {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+const playShortAudio = (): void => {
+  playAudio(shortSounds);
+};
+const playLongAudio = (): void => {
+  playAudio(longSounds);
+};
+
+export { playShortAudio, playLongAudio };
