4c04e07ebb92431607915e27bd49477c2e633367
Alicia G <alicia.guardenoalbertos@gmail.com>
2020-05-26 18:43:55 +0200

    upload basic project

---
+++ src/ts/index.ts
@@ -0,0 +1,18 @@
+// import convertMessage from "./morse-conversion";
+
+const startPatorse = (): void => {
+  let btn = (<HTMLInputElement>document.getElementById("cuackbutton"));
+  btn.addEventListener("click", (e: Event) => {
+    handleInput();
+    btn.disabled = true;
+  });
+};
+
+const handleInput = (): void => {
+  const message = (<HTMLInputElement>document.getElementById("stringToMorse")).value;
+  // const convertedMessage = convertMessage(message);
+  console.log(message);
+  // console.log(convertedMessage);
+};
+
+startPatorse();
