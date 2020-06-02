5c329470b7cbd95950a45dcc9c3e2653b555dcb2
Alicia G <alicia.guardenoalbertos@gmail.com>
2020-06-02 17:47:31 +0200

    add formatting and normalizing of text with accents and emojis

--- src/ts/morse-conversion.ts
+++ src/ts/morse-conversion.ts
@@ -1,21 +1,41 @@
 import morseAlphabet from "./morse-alphabet";
 
 const convertMessage = (originalMessage: string): string => {
-  return originalMessage
+  const formattedMessage = formatMessage(originalMessage);
+  return formattedMessage
     .slice()
     .toLowerCase()
     .split(" ")
-    .map(element => (element = wordToMorse(element)))
+    .map((element) => (element = wordToMorse(element)))
     .join("#") // symbol for between words
     .concat("$"); // symbol for end of transmission
 };
 
+const formatMessage = (originalMessage: string): string => {
+  const unified_accents = "/[\u0300-\u036f]/";
+  const unified_emoji_ranges = [
+    "\ud83c[\udf00-\udfff]", // U+1F300 to U+1F3FF
+    "\ud83d[\udc00-\ude4f]", // U+1F400 to U+1F64F
+    "\ud83d[\ude80-\udeff]", // U+1F680 to U+1F6FF
+  ];
+
+  const replaceAccentsAndEmojis = new RegExp(
+    [, unified_accents, ...unified_emoji_ranges].join("|"),
+    "g"
+  );
+
+  return originalMessage
+    .replace(/^\s+|\s+$/g,"")
+    .normalize("NFD")
+    .replace(replaceAccentsAndEmojis, "");
+};
+
 const wordToMorse = (word: string): string => {
   return word
-  .slice()
-  .split("")
-  .map(element => (element = charToMorse(element)))
-  .join("%"); // symbol for between characters of a word
+    .slice()
+    .split("")
+    .map((element) => (element = charToMorse(element)))
+    .join("%"); // symbol for between characters of a word
 };
 
 const charToMorse = (char: string): string => {
