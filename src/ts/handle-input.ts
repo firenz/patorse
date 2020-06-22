435886dd3a88fe23da1a8f81b298d5066be05d3b
Alicia G <alicia.guardenoalbertos@gmail.com>
2020-06-22 21:53:21 +0200

    add a little bit of accessibility, styling & interactivity to sliders

--- src/ts/handle-input.ts
+++ src/ts/handle-input.ts
@@ -3,11 +3,17 @@ import startPatorseAudio from "./morse-transmission";
 
 const btn = <HTMLInputElement>document.getElementById("cuack-btn");
 const textarea = <HTMLInputElement>document.getElementById("message");
+const volumeSlider = <HTMLInputElement>document.getElementById("volume");
+const volumeIcon = <HTMLSpanElement>document.getElementById("volume-icon");
 
 const initListeners = (): void => {
   btn.addEventListener("click", (e: Event) => {
     clickButton();
     btn.disabled = true;
+  });
+  volumeSlider.addEventListener("input", (e: Event) => {
+    changeVolumeIcon(volumeSlider.value);
+    // console.log(volumeSlider.value);
   });
 };
 
@@ -17,4 +23,20 @@ const clickButton = (): void => {
   startPatorseAudio(convertedMessage);
 };
 
-export {initListeners, btn, textarea};
+const changeVolumeIcon = (value: string): void => {
+  const numberValue = parseFloat(value);
+  if (numberValue > 0 && numberValue < 0.50) {
+    volumeIcon.textContent = String.fromCodePoint(0x1F508);
+  }
+  else if (numberValue >= 0.50 && numberValue < 1.50) {
+    volumeIcon.textContent = String.fromCodePoint(0x1F509);
+  }
+  else if (numberValue >= 1.50) {
+    volumeIcon.textContent = String.fromCodePoint(0x1F50A);
+  }
+  else {
+    volumeIcon.textContent = String.fromCodePoint(0x1F507);
+  }
+}
+
+export {initListeners, btn, textarea, volumeSlider, volumeIcon};
